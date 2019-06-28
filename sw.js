2e12bed146b38d4868aef14e75a7144c75c6f904
sakash1ta90 <sakashita@creaith.jp>
2019-06-28 07:48:18 +0000

    chore: update [skip ci]

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-importScripts('/<repository-name>/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/pf/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/<repository-name>/_nuxt/0a1e05b95d600de935d7.js",
-    "revision": "5a9bfe3e85542626d8f137fc6593c85c"
+    "url": "/pf/_nuxt/431c017a654a5113508f.js",
+    "revision": "a36b0bd96f1c91a72d59f98ed603a430"
   },
   {
-    "url": "/<repository-name>/_nuxt/7ebaa8ffbbd97f921c5f.js",
+    "url": "/pf/_nuxt/7ebaa8ffbbd97f921c5f.js",
     "revision": "75ee6b4d4e52d18fb4ad4504bd3f45bc"
   },
   {
-    "url": "/<repository-name>/_nuxt/9766ca1f424129ed6f44.js",
-    "revision": "a7facf67d8cce68e6ab1e526977a3b88"
+    "url": "/pf/_nuxt/f0b24095be16d39e06fc.js",
+    "revision": "962b96cc4775eced425aca22083b2247"
   },
   {
-    "url": "/<repository-name>/_nuxt/f0b24095be16d39e06fc.js",
-    "revision": "962b96cc4775eced425aca22083b2247"
+    "url": "/pf/_nuxt/f34c40bdd2a84bb6ccc0.js",
+    "revision": "84b3213d3f18a8bd4683ba558f48caab"
   }
 ], {
   "cacheId": "pf",
@@ -26,6 +26,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/<repository-name>/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/pf/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/<repository-name>/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/pf/.*'), workbox.strategies.networkFirst({}), 'GET')
