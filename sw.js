6b0889c9d4ad3ea8489e685a9ac9dddeeb174fa1
sakash1ta90 <sakashita@creaith.jp>
2019-06-28 08:37:44 +0000

    chore: update [skip ci]

--- sw.js
+++ sw.js
@@ -2,20 +2,24 @@ importScripts('/pf/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/pf/_nuxt/431c017a654a5113508f.js",
-    "revision": "a36b0bd96f1c91a72d59f98ed603a430"
+    "url": "/pf/_nuxt/16fbc48b8aa27a846457.js",
+    "revision": "4f59f682172058e6aa7cfda13d9af505"
   },
   {
-    "url": "/pf/_nuxt/7ebaa8ffbbd97f921c5f.js",
-    "revision": "75ee6b4d4e52d18fb4ad4504bd3f45bc"
+    "url": "/pf/_nuxt/ab0ff4644670e09c3745.js",
+    "revision": "aa7bfde675536d3defb081d6b4c3387c"
+  },
+  {
+    "url": "/pf/_nuxt/b5a997e8bba12d23a822.js",
+    "revision": "8089b576255d84244c262ed40e364c78"
+  },
+  {
+    "url": "/pf/_nuxt/c2d03bd7ed51fdf5ce84.js",
+    "revision": "d826394ea5d73e3252d86c0d7a483260"
   },
   {
     "url": "/pf/_nuxt/f0b24095be16d39e06fc.js",
     "revision": "962b96cc4775eced425aca22083b2247"
-  },
-  {
-    "url": "/pf/_nuxt/f34c40bdd2a84bb6ccc0.js",
-    "revision": "84b3213d3f18a8bd4683ba558f48caab"
   }
 ], {
   "cacheId": "pf",
